06b10df5842a87f1a42c419a093c14d192f7ab40
Tom Dhanabhon <thanaphon.spk@gmail.com>
2022-10-27 20:31:26 +0700

    Add DeskBand Control

---
+++ WinNetSpeedMonitor/NetSpeedDeskBand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using CSDeskBand;
+
+namespace WinNetSpeedMonitor
+{
+    [ComVisible(true)]
+    [Guid("1DB5314E-6AFB-4F31-8C52-0898620C3088")]
+    [CSDeskBandRegistration(Name = "WinNetSpeedMonitor", ShowDeskBand = true)]
+    public class NetSpeedDeskBand : CSDeskBand.CSDeskBandWin
+    {
+        private static Control _control;
+    }
+}
